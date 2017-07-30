969eb44cfbaf436eac5484d4f805a2c3048938c9
jeffecky <a51xiguanziji@163.com>
2017-07-30 22:58:00 +0800

    add unittest

--- test_django.py
+++ test_django.py
@@ -1,9 +1,28 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+import unittest
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		print('setup...')
+		self.browser = webdriver.Firefox()
+	def tearDown(self):
+		print('teardown..')
+		self.browser.quit()
 
-assert 'Django' in browser.title
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		print('test can start a list ...')
+		self.browser.get('http://localhost:8000')
+		#title是否包含To-do
+		self.assertIn('To-do', self.browser.title)
+		self.fail('Finish the test')
+
+
+print(__name__)
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
 
 # 2017.7.30
+
+
